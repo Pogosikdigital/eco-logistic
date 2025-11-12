c01f805b85f2d6dc9b487c2c324e3a1d3f501fc0
Denys <dreaaam777@gmail.com>
2025-11-12 14:59:53 -0500

    Initial project setup with SEO and hero animation

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { HelmetProvider } from "react-helmet-async";
+import "./components/styles/hero.css"; // ✅ правильный путь к твоему css
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
+);
